49aa67a3d8d54937cbc905ca35c6db64b8103e88
MarcosBombalas <ma.bombalas@duocuc.cl>
2023-06-13 22:09:02 -0400

    racismo

--- petit_cherie/petit_cherie/urls.py
+++ petit_cherie/petit_cherie/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tienda.views import index
+	
 
 urlpatterns = [
     path('admin/', admin.site.urls),
