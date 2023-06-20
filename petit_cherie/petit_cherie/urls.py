e19fc550f6064403cc87dfca082c1acd0a68de08
eypino <107155607+eypino@users.noreply.github.com>
2023-06-20 10:44:17 -0400

    union de codigos

--- petit_cherie/petit_cherie/urls.py
+++ petit_cherie/petit_cherie/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tienda.views import index,carrito,login,tienda,registro,panaderia,pasteleria,tortas,quienesSomos,recuperarContrasena,recuperarCuenta
+from tienda.views import index,carrito,login,tienda,registro,panaderia,pasteleria,tortas,quienesSomos,recuperarContrasena,recuperarCuenta,incercion,editar,eliminar
 	
 
 urlpatterns = [
@@ -32,4 +32,7 @@ urlpatterns = [
     path('quienes_somos',quienesSomos,name='quienesSomos'),
     path('recuperar_contrasena',recuperarContrasena,name='recuperarContrasena'),
     path('recuperar_cuenta',recuperarCuenta,name='recuperarCuenta'),
+    path('incercion',incercion,name='incercion'),
+    path('editar',editar,name='editar'),
+    path('eliminar',eliminar,name='eliminar'),
 ]
