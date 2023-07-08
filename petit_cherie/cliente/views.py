6f4162042ceb0195c434c07a66278c2afcb85ead
eypino <107155607+eypino@users.noreply.github.com>
2023-07-08 16:56:42 -0400

    autenticación corregida

--- petit_cherie/cliente/views.py
+++ petit_cherie/cliente/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login as auth_login  
 
-def login_view(request):  
-    context = {}
-    return render(request, 'registration/login.html', context)    
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # Redirige a la página de inicio después del inicio de sesión exitoso
+        else:
+            # El usuario no es válido, mostrar un mensaje de error
+            context = {'error_message': 'Credenciales inválidas'}
+            return render(request, 'registration/login.html', context)
+    else:
+        context = {}
+        return render(request, 'registration/login.html', context)
+  
 
 def recuperarContrasena(request):
     context = {}
